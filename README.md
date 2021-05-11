da2c7654dc5edf1df73d8b6d6d6ee590dc87b304
Joe Snikeris <jsnikeris@Devons-MacBook-Pro.local>
2021-05-11 08:30:04 -0400

    Create empty project w/ leiningen

---
+++ README.md
@@ -0,0 +1,26 @@
+# rate
+
+FIXME: description
+
+## Installation
+
+Download from http://example.com/FIXME.
+
+## Usage
+
+FIXME: explanation
+
+    $ java -jar rate-0.1.0-standalone.jar [args]
+
+## Options
+
+FIXME: listing of options this app accepts.
+
+## Examples
+
+...
+
+### Bugs
+
+...
+
---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject rate "0.1.0-SNAPSHOT"
+  :description "Rate.com Evaluation"
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/java.jdbc "0.7.12"]]
+  :main ^:skip-aot rate.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+
+
